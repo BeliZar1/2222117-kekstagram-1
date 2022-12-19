7f716a508e5b6cb717a2a896b102936a3acccad6
BeliZar1 <entin2003@gmail.com>
2022-12-19 21:26:24 +0500

    module12-task1

--- js/util.js
+++ js/util.js
@@ -1,35 +1,60 @@
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
-
-const checkStringLength = (checkingString, maxLength) => checkingString.length <= maxLength;
-
-const getCommentId = (commentId) => {
-  if (commentId === 0) {
-    return 0;
-  }
-  return commentId - 1;
-};
-
-const getDescriptionId = (discriptionId) => {
-  if (discriptionId === 0) {
-    return 0;
-  }
-  return discriptionId - 1;
-};
+import {addThumbnails} from './thumbnails.js';
+import {setFilter, showFilters, TIMEOUT_DELAY} from './filters.js';
+import {thumbnailClickHandler} from './pictures.js';
 
 const isEscapeKey = (event) => event.key === 'Escape';
 
-export {
-  getRandomPositiveInteger,
-  getRandomArrayElement,
-  checkStringLength,
-  getDescriptionId,
-  getCommentId,
-  isEscapeKey,
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
 };
+
+const shuffleArray = (array) => {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+};
+
+const onSuccess = (data) => {
+  const photos = data.slice();
+  addThumbnails(photos);
+  showFilters();
+  setFilter(debounce((filterData) => addThumbnails(filterData(data)), TIMEOUT_DELAY));
+  thumbnailClickHandler(data);
+  document.querySelector('.img-filters').classList.remove('img-filters--inactive');
+};
+
+const onFail = () => {
+  const messageAlert = document.createElement('div');
+  messageAlert.style.position = 'absolute';
+  messageAlert.style.left = 0;
+  messageAlert.style.top = 0;
+  messageAlert.style.right = 0;
+  messageAlert.style.textAlign = 'center';
+  messageAlert.style.fontSize = '30px';
+  messageAlert.style.backgroundColor = 'red';
+  messageAlert.style.padding = '10px 5px';
+  messageAlert.textContent = 'Ошибка загрузки данных';
+  document.body.append(messageAlert);
+};
+
+export {
+  isEscapeKey,
+  onSuccess,
+  onFail,
+  shuffleArray
+};
+
